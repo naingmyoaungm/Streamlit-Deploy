b4761360f004d4c67a0b135aac24be85e165e3bc
Naing Myo Aung <123310276+naingmyoaungm@users.noreply.github.com>
2024-05-15 16:04:56 +0630

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+st.title('This is :green[My Assignment]')
+st.title(':red[_Streamlit_] is :blue[cool] :sunglasses:')
+
+number = st.number_input("Insert a number")
+st.write("The current number is ", number)
+
+st.write("The answer of mutiple '7' is ",7*number)
